77c40a501e8c9b2076fdc8f6af5679e794f76290
ashot15 <38916348+ashot15@users.noreply.github.com>
2023-12-05 21:32:39 +0500

    Add files via upload
    угадывание числа

---
+++ угадайка.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+
+namespace consoleApp2
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("Введи свое имя:\t");
+            string name = Console.ReadLine();
+            link1:
+            Console.Write("Попробуй угадать число:\t");
+            string input = Console.ReadLine();
+            if (input == "1")
+                Console.WriteLine($"Молодец {name}");
+            else
+                Console.WriteLine("Ты не угадал, попробуй снова");
+            if (input !="1") goto link1;
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+        
+
+
+    }
+
+
+  
+
+
+
+}
+
+
+
+
+
+
+
+
+
+

791e8b34bd978b3cf5de354cb7913376cf818a5a
ashot15 <38916348+ashot15@users.noreply.github.com>
2023-12-07 00:34:49 +0500

    Add files via upload

---
+++ Вывод информации о системе.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            bool is64 = Environment.Is64BitOperatingSystem;
+            int time_windows = Environment.TickCount ;
+            var os = Environment.OSVersion;
+            Console.WriteLine($"Текущая дата {DateTime.Now}");
+            Console.WriteLine($"Операционная система: {os}");
+            Console.WriteLine($"Количество потоков у процессора: {Environment.ProcessorCount}");
+            Console.WriteLine($"Имя машины: {Environment.MachineName}");
+            Console.WriteLine($"Версия: {Environment.Version}");
+            Console.WriteLine($"Как давно работает Windows в днях : {time_windows / 86400}");
+
+            
+            {
+                if (is64 == true)
+                
+                    Console.WriteLine($"64 Битная версия Windows : Да");
+
+
+
+
+
+
+                 else if (is64 == false) {
+                        Console.WriteLine($"64 Битная версия Windows : Нет");
+
+                    }
+                
+
+
+            }
+
+
+
+
+
+
+
+        }
+    }
+}
